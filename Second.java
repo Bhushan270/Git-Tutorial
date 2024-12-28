1ee2f547c78d103c6485e2d01fd2496b76b3940a
Bhushan <bhushangawand442@gmail.com>
2024-12-28 20:15:23 +0530

    Version 3

--- Second.java
+++ Second.java
@@ -2,6 +2,6 @@ public class Second {
     public static void main(String[] args) {
 
         System.out.println("second");
-        System.out.println("version 2");
+        System.out.println("version 3");
     }
 }
